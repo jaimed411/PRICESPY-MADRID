08f0d24db74330d81776826a3aff78b9fd49eeb2
Jaime Dodero <131529197+jaimed411@users.noreply.github.com>
2023-10-18 17:33:34 +0000

    .

--- src/app.py
+++ src/app.py
@@ -10,41 +10,29 @@ model = joblib.load(r'/workspaces/probando/data/MODEL3.sav')
 def predict_price(features):
     return model.predict([features])[0]
 
-# Configurar el tema de la aplicación con colores fríos
-st.set_page_config(
-    page_title="Predicción de Precio de Propiedades",
-    page_icon="🏠",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
-
 # Encabezado de la aplicación con un título atractivo
-st.title('Predicción de Precio de Propiedades en Madrid')
-
-# Sidebar con opciones de entrada y colores personalizados
-st.sidebar.header('Ingrese las características de la propiedad')
-st.sidebar.markdown('**Por favor, complete los siguientes campos:**')
+st.title('Precio medio en Madrid')
 
 # Crear widgets para las características de entrada
-euros_per_m2 = st.sidebar.number_input('Euros/m2', min_value=0, max_value=10000, step=1)
-baños = st.sidebar.number_input('Baños', min_value=0, max_value=10, step=1)
-metros_cuadrados = st.sidebar.number_input('Metros cuadrados construidos', min_value=0, max_value=1000, step=1)
-habitaciones = st.sidebar.number_input('Habitaciones', min_value=0, max_value=10, step=1)
-planta = st.sidebar.number_input('Planta', min_value=0, max_value=10, step=1)
+euros_per_m2 = st.number_input('Euros/m2', min_value=0, max_value=10000, step=1)
+baños = st.number_input('Baños', min_value=0, max_value=10, step=1)
+metros_cuadrados = st.number_input('Metros cuadrados construidos', min_value=0, max_value=1000, step=1)
+habitaciones = st.number_input('Habitaciones', min_value=0, max_value=10, step=1)
+planta = st.number_input('Planta', min_value=0, max_value=10, step=1)
 
 # Crear un mapeo para mostrar "Sí" y "No" en lugar de 0 y 1
 si_no_mapping = {0: "No", 1: "Sí"}
 
 # Utilizar el mapeo en los selectboxes
-terraza = st.sidebar.selectbox('Terraza', [0, 1], format_func=lambda x: si_no_mapping[x])
-aire_acondicionado = st.sidebar.selectbox('Aire acondicionado', [0, 1], format_func=lambda x: si_no_mapping[x])
-trastero = st.sidebar.selectbox('Trastero', [0, 1], format_func=lambda x: si_no_mapping[x])
-piscina = st.sidebar.selectbox('Piscina', [0, 1], format_func=lambda x: si_no_mapping[x])
-parking_incluido = st.sidebar.selectbox('Parking incluído', [0, 1], format_func=lambda x: si_no_mapping[x])
-jardin = st.sidebar.selectbox('Jardín', [0, 1], format_func=lambda x: si_no_mapping[x])
-parking = st.sidebar.selectbox('Parking', [0, 1], format_func=lambda x: si_no_mapping[x])
-ascensor = st.sidebar.selectbox('Ascensor', [0, 1], format_func=lambda x: si_no_mapping[x])
-obra_nueva = st.sidebar.selectbox('Obra nueva', [0, 1], format_func=lambda x: si_no_mapping[x])
+terraza = st.selectbox('Terraza', [0, 1], format_func=lambda x: si_no_mapping[x])
+aire_acondicionado = st.selectbox('Aire acondicionado', [0, 1], format_func=lambda x: si_no_mapping[x])
+trastero = st.selectbox('Trastero', [0, 1], format_func=lambda x: si_no_mapping[x])
+piscina = st.selectbox('Piscina', [0, 1], format_func=lambda x: si_no_mapping[x])
+parking_incluido = st.selectbox('Parking incluído', [0, 1], format_func=lambda x: si_no_mapping[x])
+jardin = st.selectbox('Jardín', [0, 1], format_func=lambda x: si_no_mapping[x])
+parking = st.selectbox('Parking', [0, 1], format_func=lambda x: si_no_mapping[x])
+ascensor = st.selectbox('Ascensor', [0, 1], format_func=lambda x: si_no_mapping[x])
+obra_nueva = st.selectbox('Obra nueva', [0, 1], format_func=lambda x: si_no_mapping[x])
 
 # Crear un diccionario con las características ingresadas
 input_features = {
@@ -71,11 +59,5 @@ else:
     predicted_price = predict_price(list(input_features.values()))
 
 # Mostrar el precio predicho en la aplicación con estilo
-st.subheader('Precio Estimado de la Propiedad')
 st.write(f'El precio estimado de la propiedad es: {predicted_price:.2f} Euros')
-st.markdown('**Nota:** Los resultados son estimaciones basadas en el modelo.')
 
-# Pie de página con información adicional
-st.sidebar.markdown('---')
-st.sidebar.text('Desarrollado por: Tu Nombre')
-st.sidebar.text('Fecha: Septiembre 2023')
